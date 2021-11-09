da077487178e64f1bd028f021c010b776ea3d651
Bonbec <cseghir36@gmail.com>
2021-11-09 20:40:48 +0100

    Graphics

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import math
 import statistics
-from src.DataArray import DataArray
-from src.ClassData import ClassData
-
-
+import matplotlib.pyplot as plt
+import numpy as np
+from DataArray import DataArray
+from ClassData import ClassData
 def ecart_centre_red(a, mean, ecart_type):
     b = (a - mean) / ecart_type
     return b
@@ -56,7 +56,8 @@ for i in range(class_count):
     class_data.append(data)
 # On ajoute 1 pour faire comme si l'intervalle était fermé. On aurait pu ajouter 2, 3, 4, 0.0000001,
 # mais autant ajouter 1, c'est pratique ^^
-class_data[-1].supremum = values[-1] + 1
+#class_data[-1].supremum = values[-1] + 1
+#J'ai pas compris ici, mais ça fesait un dernier intervalle incoherent
 
 
 for i, data in enumerate(class_data):
@@ -72,12 +73,56 @@ for data in class_data:
 xhi2 = class_count * sum(
     [(data.experimental_frequency * data.theorical_frequency) ** 2 / data.theorical_frequency for data in class_data])
 
+bins_class = [] #On definis les diffférents intervalles
+for data in class_data:
+    bins_class.append(data.infimum)
+bins_class.append(values[-1])
+
+fig, ax = plt.subplots() #On cree la fenetre pyplot
+exp_freq_array = np.array(values)#On change une liste en une matrice
+
 print("Valeurs centrées réduites : " + str(values))
 print("Etendue des valeurs : " + str(width))
 print("Longueur des classes : " + str(class_width))
 print("Données des classes : " + str(class_data))
 print("xhi² : " + str(xhi2))
+#10,10,11,11,11,13,12,9,12,14
+#Echantillon test1
+x = []
+y = []
+#On cree une liste d'antécendant une une matrice d'immage pour pouvoir
+#creer la courbe théorique
+while len(x) <= 100:
+    x.append((sample[0]-sample_mean)+len(x)*(sample[-1]-sample[0])/100)
+for i in range(len(x)):
+    y.append((1 / (np.sqrt(2 * np.pi) * e_type_sb)) * np.exp(-0.5 * ((x[i]) / e_type_sb) ** 2))
 
+num_bins = class_count
+#On construit l'histogramme reel
+n, bins, patches = ax.hist(exp_freq_array, num_bins, density=True)
+#14,15,16,17,18,14,15,17,16,17,17,18,17,17,13,15,15,15,14,13,16,17,18,14,15,16,17,18,19,17,14,17,16,15,15,16
+#echantillon test 2
+#0.385,0.387,0.388,0.383,0.391,0.380,0.382,0.384,0,.386,0.389,0.390,0.385,0.389,0.388,0.382,0.387,0.388,0.392,0.383,0.388,0.385
+#385,387,388,383,391,380,382,384,386,389,390,385,389,388,382,387,388,392,383,388,385
+#On transforme les liste en matrice
+x_array = np.array(x)
+y_array = np.array(y)
+#On met le teste de la bulle
+textstr = "$\mu=%.2f${}\n$\sigma=%.2f${}\n\u03C7\u00B2={}".format(round(sample_mean, 3), round(e_type_sb, 3), round(xhi2, 4))
+#On creer la bulle
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+#On assemble le texte et la bulle pour former la légende
+ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14, verticalalignment='top', bbox=props)
+
+#On creer la courbe théorique
+plt.plot(x_array, y_array, '--')
+#On met le titre
+plt.title("Test du \u03C7\u00B2")
+#On met une legende sur l'axe y
+plt.ylabel('Frequence')
+
+
+plt.show()
 
 
 #print("La moyenne de l'echantillon est :", format(moy_sample))
